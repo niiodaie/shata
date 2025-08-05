b0b307eecf021966a92aeba061e70d213acf4bfb
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-05 06:03:58 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html',
-    },
-  },
-});
+})
